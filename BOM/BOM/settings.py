e37cacb2751cc141af919ab8ae8c5bd860810381
dibyendu78 <dibyendupatra2019@gmail.com>
2026-07-06 07:46:35 +0530

    done

--- BOM/BOM/settings.py
+++ BOM/BOM/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'Account.User'
 LOGIN_URL = '/coordinator/login/'
